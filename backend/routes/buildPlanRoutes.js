5ac7c40400cdadf6c3902d76be6da37005c19b68
dizzydog-rgb <fox29582@yahoo.com.tw>
2024-10-17 11:02:29 +0800

    server更新

--- backend/routes/buildPlanRoutes.js
+++ backend/routes/buildPlanRoutes.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const router = express.Router();
 
 // 假設這是一些計畫的假資料
@@ -8,51 +8,51 @@ const router = express.Router();
 //     { id: 2, planName: 'second Plan', startDate: '2024/10/15', endDate: '2024/10/18' },
 //     { id: 3, planName: 'Third Plan', startDate: '2024/11/25', endDate: '2024/11/27' }
 //   ];
-  
-  // GET 請求: 取得頁面
-router.get('/create', function (req, res) {
-    const options = {
-        root: path.join(__dirname, '../../', 'dist')
-    };
-  
-    const fileName = 'buildPlan.html';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            console.error('Error sending file:', err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
-  });
 
-  router.get('/edit', function (req, res) {
-    const options = {
-        root: path.join(__dirname, '../../', 'dist')
-    };
-  
-    const fileName = 'editPlan.html';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            console.error('Error sending file:', err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
-  });
+// GET 請求: 取得頁面
+router.get("/create", function (req, res) {
+  const options = {
+    root: path.join(__dirname, "../../", "dist"),
+  };
 
-  router.get('/list', function (req, res) {
-    const options = {
-        root: path.join(__dirname, '../../', 'dist')
-    };
-  
-    const fileName = 'planList.html';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            console.error('Error sending file:', err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
+  const fileName = "buildPlan.html";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.error("Error sending file:", err);
+    } else {
+      console.log("Sent:", fileName);
+    }
   });
+});
+
+router.get("/edit", function (req, res) {
+  const options = {
+    root: path.join(__dirname, "../../", "dist"),
+  };
+
+  const fileName = "editPlan.html";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.error("Error sending file:", err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
+
+router.get("/list", function (req, res) {
+  const options = {
+    root: path.join(__dirname, "../../", "dist"),
+  };
+
+  const fileName = "planList.html";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.error("Error sending file:", err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
+});
 
 module.exports = router;
