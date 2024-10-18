f75ac040ab8e5f6da9009d37f6e95f4bda7af0c4
hua-00 <zxc5566230@gmail.com>
2024-10-18 16:53:39 +0800

    Server連線完成、成功渲染一小部分資料到畫面

--- backend/routes/PopupBudgetRoutes.js
+++ backend/routes/PopupBudgetRoutes.js
@@ -8,8 +8,14 @@ router.use(cors());
 // console.log("Current directory:", __dirname);
 // console.log("Trying to require PopupBudgetController from:", "../controllers/PopupBudgetController");
 
-const PopupBudgetController = require("../controller/PopupBudgetController");
+// 呼叫控制器
+const popupBudgetController = require("../controller/popupBudgetController");
+
 
 // 定義 GET 請求的路由為 /sites/:id，來處理帶有景點 ID 的請求
-router.get("/popupbudget/:id", PopupBudgetController.getPopupBudgetById);
+// 這裡定義各種資料取用的路由請求
+router.get("/popupBudget/:id", popupBudgetController.getPopupBudgetById);
+router.get("/popupBudgets", popupBudgetController.getAllPopupBudgets);
+
+
 module.exports = router;
