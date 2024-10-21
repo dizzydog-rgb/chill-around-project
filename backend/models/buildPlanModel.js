e2edd151b89bd07f80220b1dc1d40396878cb041
Jackie_Chen <fox29582@yahoo.com.tw>
2024-10-22 00:17:46 +0800

    根據不同天數過濾資料並渲染當天的景點

--- backend/models/buildPlanModel.js
+++ backend/models/buildPlanModel.js
@@ -3,7 +3,7 @@ const db = require("../config/database");
 // 獲取所有行程的模組函數
 exports.findAllSchedule = () => {
   return new Promise((resolve, reject) => {
-    const query = "SELECT * FROM `schedule`";
+    const query = "SELECT * FROM schedule";
     db.exec(query, [], (error, results, fields) => {
       if (results) {
         resolve(results);
@@ -23,6 +23,23 @@ exports.findScheduleById = (id) => {
     FROM schedule s
     JOIN schedule_details sd ON s.sch_id = sd.sch_id
     WHERE s.sch_id = ?;
+  `;
+    db.exec(query, [id], (error, results, fields) => {
+      if (results) {
+        resolve(results);
+      } else {
+        console.error("No results found or query error");
+        reject(new Error("No results found or query error"));
+      }
+    });
+  });
+};
+
+// 添加景點至特定編號行程的模組函數
+exports.addSiteToSchedule = (id) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+    SQL
   `;
     db.exec(query, [id], (error, results, fields) => {
       if (results) {
--- backend/routes/buildPlanRoutes.js
+++ backend/routes/buildPlanRoutes.js
@@ -3,8 +3,14 @@ const path = require("path");
 const router = express.Router();
 const buildPlanController = require("../controller/buildPlanController");
 
-// GET 請求: 呼叫controller
+// 呼叫controller
+// GET 請求: 獲取所有行程資料
 router.get("/planList", buildPlanController.getAllSchedule);
+// GET 請求: 獲取特定ID的行程資料
 router.get("/editPlan/:id", buildPlanController.getScheduleById);
+// POST 請求: 新增特定ID行程的景點
+router.post("/editPlan/sites/:id", buildPlanController.postSiteToSchedule);
+// PUT 請求: 修改特定ID行程
+// router.put("/editPlan/:id", buildPlanController.getScheduleById);
 
 module.exports = router;
