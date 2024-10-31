e30575ff2c17bf31d161bfaeeb2e5c3a4dc6c5b7
Jackie_Chen <fox29582@yahoo.com.tw>
2024-10-31 12:28:30 +0800

    更新planList的地圖功能

--- js/editPlan.js
+++ js/editPlan.js
@@ -135,7 +135,7 @@ function renderEditPlan(schedules) {
   // 切換不同的天數
   const dayListItems = dayList.getElementsByTagName("li");
   for (let i = 0; i < dayListItems.length - 1; i++) {
-    dayListItems[i].addEventListener("click", () =>
+    dayListItems[i].addEventListener("click", () => 
       switchCurrentDay(i, schedules)
     );
   }
